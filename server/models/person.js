85885af0b02191db2daaff37885d98e6af6853f6
jakeiwen <iwenx006@umn.edu>
2016-12-05 09:50:29 -0600

    basic

---
+++ server/models/person.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// step 1: create the Schema
+var personSchema = new Schema({
+  name: {type: String, required: true},
+  location: String,
+  birthDate: Date,
+  nicenessLevel: { required: true, type: Number, min: 0, max: 5 }
+});
+
+personSchema.pre('save', function(next) {
+  next();
+});
+
+// step 2 - create the model
+var Person = mongoose.model('Person', personSchema);
+
+// step 3 - export our model
+module.exports = Person;
